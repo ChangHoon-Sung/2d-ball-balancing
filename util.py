1a886fb38f19dd56bc4b3984a3eef4e8619311c8
Changhoon Sung <changhoon.dev@gmail.com>
2023-12-18 13:34:14 +0900

    Change white lower limit

--- util.py
+++ util.py
@@ -3,5 +3,5 @@ class RGB:
     GREEN = (0, 255, 0)
     YELLOW = (0, 255, 255)
     BLUE = (255, 0, 0)
-    WHITELOWER = (180, 180, 180)
+    WHITELOWER = (190, 190, 190)
     WHITE = (255, 255, 255)
